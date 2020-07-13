52939590671bf1a40f20baeb2e649d162ab0fcc2
alexdev06 <alexandre.manteaux@gmail.com>
2020-07-13 15:02:57 +0200

    Minors updates on UserType forms

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -20,18 +20,22 @@ class UserType extends AbstractType
         $builder
             ->add('username', TextType::class, ['label' => "Nom d'utilisateur"])
             ->add('roles', ChoiceType::class, [
-                    'choices' => [
-                        'Admin' => 'ROLE_ADMIN',
-                        'Utilisateur' => 'ROLE_USER'
-                    ],
-                    'expanded'  => false
+                'choices' => [
+                    'Admin' => 'ROLE_ADMIN',
+                    'Utilisateur' => 'ROLE_USER'
+                ],
+                'expanded'  => false
             ])
             ->add('password', RepeatedType::class, [
                 'type' => PasswordType::class,
                 'invalid_message' => 'Les deux mots de passe doivent correspondre.',
                 'required' => true,
-                'first_options'  => ['label' => 'Mot de passe'],
-                'second_options' => ['label' => 'Tapez le mot de passe à nouveau'],
+                'first_options'  => [
+                    'label' => 'Mot de passe'
+                ],
+                'second_options' => [
+                    'label' => 'Tapez le mot de passe à nouveau'
+                ]
             ])
             ->add('email', EmailType::class, ['label' => 'Adresse email'])
         ;
